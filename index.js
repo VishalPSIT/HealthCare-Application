3967e760dd15804b64f354adb053e95b155de8dd
Vishal Raj <vraj70404@gmail.com>
2024-08-02 22:59:12 +0530

    craeted dev branch and comments in index.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const {connectDb} = require("./config/database.js");
+
+//configuring env
 dotenv.config({
     path: "./config/config.env"
 })
 
+// constructing express app
 const app = express();
 
-
+// middlewares.
 app.use(express.json());
 app.use(express.urlencoded({
     extended:true
@@ -17,9 +20,13 @@ app.use(express.urlencoded({
 app.use(cookieParser);
 app.use(cors);
 
+//connecting database.
 connectDb();
 
+// geting port
 const port = process.env.PORT || 8080; 
+
+// express app listening at 3000 || 8080 port.
 app.listen(port,()=>{
     console.log(`server runnig at https://localhost/${port}`);
 })
