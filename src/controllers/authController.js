3c0c0bcd665ccb6d58fc2b43bb9aa5e86507aa38
VishalPSIT <vishal.psit2213663@gmail.com>
2024-08-05 13:13:50 +0530

    changes applied

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
 const {successResponse} = require("../utils/apiResponse.js");
 const user = require("../models/user.js");
-const doctor = require("../models/doctor.js")
+const Doctor = require("../models/doctor.js");
+const Hospital = require("../models/hospial.js")
 
 
 
 exports.userSignUp = async(req,res)=>{
-
     try{
         //get user details
         const {name , email , dateOfBirth , gender , phone} = req.body;
 
-        //validate user details
+        //validate user details 
         if (name && email && dateOfBirth && gender && phone){
             //validation success ->pending
 
@@ -29,9 +29,6 @@ exports.userSignUp = async(req,res)=>{
                 const jsonObj = {success:false,message:"User already exists"}
                 successResponse(req,res,400,jsonObj);
             }
-            
-
-
         }
         else{
             //data is missing
@@ -115,3 +112,75 @@ exports.userUpdateProfile = async (req, res) => {
         successResponse(req, res, 500, jsonObj);
     }
 };
+
+
+//-----------------------------------------------(Hospital Details) ---------------------------------------
+
+exports.hospitalSignUp = async(req,res)=>{
+    try{
+        //get user details
+        const {hospital_name , email , type , beds_available , address , password} = req.body;
+
+        //validate user details 
+        if (hospital_name && email && type && beds_available && address && password){
+            //validation success ->pending
+
+
+            //check Hospital existance
+            const existingHospital =await Hospital.findOne({email});
+            console.log("existing user is " + existingHospital);
+            if (!existingHospital){
+                //create Hospital
+                
+                const newHospital =await Hospital.create({hospital_name , email , type , beds_available , address , password});
+                const jsonObj = {success:true , message:"Hospital Created"}
+                successResponse(req,res,200,jsonObj);
+
+            }
+            else{
+                const jsonObj = {success:false,message:"User already exists"}
+                successResponse(req,res,400,jsonObj);
+            }
+        }
+        else{
+            //data is missing
+            const jsonObj = {success:false,message:"Data is missing"};
+            successResponse(req,res,400,jsonObj);
+        }
+        
+
+    }
+    catch(e){
+        const jsonObj = {success:false , message:"Something went wrong at server"}
+        successResponse(req,res,400,jsonObj);
+    }
+}
+
+
+
+
+exports.userSignIn = async (req,res) =>{
+    //req->email
+    try{
+        const {email} = req.body;
+        //validate email ->  pending
+
+        const existingUser = await user.findOne({email});
+        if (existingUser){
+            //oAuth implementation
+
+            const jsonObj ={success:true , message:"User Signed in"}
+            successResponse(req,res,200,jsonObj);
+        }
+        else{
+            const jsonObj ={success:false , message:"No user exists"}
+            successResponse(req,res,400,jsonObj);
+        }
+    }
+    catch(e){
+        console.log(e);
+        const jsonObj = {success:false , message:"Something went Wrong at Server"}
+        successResponse(req,res,400,jsonObj);
+
+    }
+}
